4a9e05ecaa10ffc4fbe8e19cb0a6961fc6a5a8f9
chenhang <1020287884@qq.com>
2019-10-15 17:57:00 +0800

    10.15日 需求明确

--- children_1688/main.py
+++ children_1688/main.py
@@ -12,15 +12,18 @@ from children_1688.spiders.annualIndex import Children01Spider
 '''
     陈航
     增加顺序执行所有spider
-
+    
+    
+    注： 如有相同spider名称
+        需要改变相同的spider名称 
+        
+        localToHdfs    将 "/home/chenhang/workplace/crawlfile"; 下的所有文件 push到103服务器上的hdfs系统中 路径为："/user/piday/zhili/chenhang";
+        localToHive    将
+        
 '''
 configure_logging()
 runner = CrawlerRunner()
 
-'''
-    注： 如有相同spider名称
-        需要改变相同的spider名称 
-'''
 
 # 我是采购商全年三大指数
 runner.crawl(Children01Spider)
@@ -35,10 +38,3 @@ d = runner.join()
 d.addBoth(lambda _: reactor.stop())
 reactor.run() # the script will block here until all crawling jobs are finished
 
-
-
-
-# os.system('scrapy crawl aliindex_30_3 -s CLOSESPIDER_TIMEOUT=300')
-# os.system('aliindex_30_3爬取完成******************************************************')
-# os.system('scrapy crawl aliindex_30_4 -s CLOSESPIDER_TIMEOUT=300')
-# os.system('aliindex_30_4爬取完成******************************************************')
