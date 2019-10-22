6c20743c71510c035a82d8d8e3198774709bf299
chenhang <1020287884@qq.com>
2019-10-22 17:26:44 +0800

    10.22日　重写完成　现在的问题还是重复公司编号２

---
+++ children_1688/spiders/alisupplyfilemarket.py
@@ -0,0 +1,77 @@
+# -*- coding: utf-8 -*-
+import scrapy
+
+from children_1688.items import aLiSupplyFileMarket_Item
+
+
+class AlisupplyfilemarketSpider(scrapy.Spider):
+    name = 'alisupplyfilemarket'
+    allowed_domains = ['alibaba.com']
+    start_urls = [
+        'https://www.alibaba.com/trade/search?spm=a2700.supplier-normal.16.4.1f86459bWExbQ1&n=38&f1=y&indexArea=company_en&viewType=L&keyword=children_clothes&page=1']
+    head = 'https://www.alibaba.com/trade/search?spm=a2700.supplier-normal.16.4.1f86459bWExbQ1&n=38&f1=y&indexArea=company_en&viewType=L&keyword=children_clothes&page='
+    next = []
+    for i in range(1, 58):
+        next.append(str(i))
+    custom_settings = {
+        'ITEM_PIPELINES': {'children_1688.pipelines.aLiSupplyFileMarket_pipelines': 300, },
+    }
+
+    def parse(self, response):
+        divs = response.xpath('.//div[@class="item-main"]')
+        companyNames = []
+        numbers = 1
+        areas = []
+        mainMarkets = []
+        items = []
+        # 获取数据
+        for div in divs:
+            companyName = div.xpath('div[1]/div[2]/div[1]/div[2]/h2/a/text()').extract()
+            if len(companyName) == 0:
+                companyName = div.xpath('div[1]/div[2]/div[1]/div[1]/h2/a/text()').extract()
+            midd = div.xpath('.//span[contains(@data-mark,"TR")]/text()').extract()
+            if len(midd) == 0:
+                mainMarkets.append(0)
+            middarea = []
+            middcount = []
+            for i in range(0,len(midd)):
+                count = midd[i].rfind(' ')
+                middarea.append(str(midd[i])[:count])
+                middcou = str(midd[i])[count+1:]
+                middcou = middcou.strip('%')
+                middcou = float(middcou)
+                middcount.append(int(middcou)/100)
+            try:
+                companyName = companyName[0]
+            except:
+                companyName = ''
+            if len(midd) >= 1 :
+                for i in range(0,len(midd)):
+                    companyNames.append(companyName)
+                    areas.append(middarea[i])
+            else:
+                companyNames.append(companyName)
+                areas.append(' ')
+            for m in middcount:
+                mainMarkets.append(m)
+
+        # 赋值数据
+        for i in range(0, len(companyNames)):
+            item = aLiSupplyFileMarket_Item()
+            item['companyName'] = companyNames[i]
+            item['number'] = numbers
+            item['area'] = areas[i]
+            item['mainMarket'] = mainMarkets[i]
+            items.append(item)
+        print(response.url)
+        surl = response.url
+        count = surl.rfind('=')
+        reurl = surl[count + 1:]
+        self.next.remove(reurl)
+        if self.next:
+            r = scrapy.Request(url=self.head + self.next[0], callback=self.parse)
+            items.append(r)
+        elif len(self.next) == 0:
+            print(
+                '更新Spider完成 , 更新数据名称 : https://www.alibaba.com/trade/search?spm=a2700.supplier-normal.16.4.9e19459b8XUEfx&viewType=L&n=50&indexArea=company_en&keyword=children_clothes&page=1&f1=y')
+        return items
---
+++ children_1688/spiders/alisupplyfilproduct.py
@@ -0,0 +1,72 @@
+# -*- coding: utf-8 -*-
+import scrapy
+
+from children_1688.items import aLiSupplyFileProduct_Item
+
+
+class AlisupplyfilproductSpider(scrapy.Spider):
+    name = 'alisupplyfilproduct'
+    allowed_domains = ['alibaba.com']
+    start_urls = [
+        'https://www.alibaba.com/trade/search?spm=a2700.supplier-normal.16.4.1f86459bWExbQ1&n=38&f1=y&indexArea=company_en&viewType=L&keyword=children_clothes&page=1']
+    head = 'https://www.alibaba.com/trade/search?spm=a2700.supplier-normal.16.4.1f86459bWExbQ1&n=38&f1=y&indexArea=company_en&viewType=L&keyword=children_clothes&page='
+    next = []
+    for i in range(1, 58):
+        next.append(str(i))
+    custom_settings = {
+        'ITEM_PIPELINES': {'children_1688.pipelines.aLiSupplyFileProduct_pipelines': 300, },
+    }
+
+    def parse(self, response):
+        divs = response.xpath('.//div[@class="item-main"]')
+        companyNames = []
+        numbers = 1
+        mainProducts = []
+        items = []
+        mainPro = []
+        # 获取数据
+        for div in divs:
+            companyName = div.xpath('div[1]/div[2]/div[1]/div[2]/h2/a/text()').extract()
+            if len(companyName) == 0:
+                companyName = div.xpath('div[1]/div[2]/div[1]/div[1]/h2/a/text()').extract()
+            mainProduct = div.xpath('.//div[@class="value ellipsis ph"]/text()').extract()
+            if len(mainProduct) > 0:
+                mainProduct = str(mainProduct)[1:-1]
+                mainProduct = mainProduct.replace('\'','').replace(' ','')
+                mainProduct = mainProduct.split(',')
+            else:
+                mainProduct = ''
+            for i in range(0,len(mainProduct)):
+                if mainProduct[i] != '':
+                    mainPro.append(mainProduct[i])
+            if 'China' in mainPro:
+                mainPro = ''
+            try:
+                companyName = companyName[0]
+            except:
+                companyName = ''
+            if len(mainPro) > 0 :
+                for i in range(0,len(mainPro)):
+                    companyNames.append(companyName)
+                    mainProducts.append(mainPro[i])
+            else:
+                companyNames.append(companyName)
+                mainProducts.append(' ')
+        # 赋值数据
+        for i in range(0, len(companyNames)):
+            item = aLiSupplyFileProduct_Item()
+            item['companyName'] = companyNames[i]
+            item['number'] = numbers
+            item['product'] = mainProducts[i]
+            items.append(item)
+        surl = response.url
+        count = surl.rfind('=')
+        reurl = surl[count + 1:]
+        self.next.remove(reurl)
+        if self.next:
+            r = scrapy.Request(url=self.head + self.next[0], callback=self.parse)
+            items.append(r)
+        elif len(self.next) == 0:
+            print(
+                '更新Spider完成 , 更新数据名称 : https://www.alibaba.com/trade/search?spm=a2700.supplier-normal.16.4.9e19459b8XUEfx&viewType=L&n=50&indexArea=company_en&keyword=children_clothes&page=1&f1=y')
+        return items
